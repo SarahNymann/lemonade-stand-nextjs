c5a44ed30c9072583ba0f021c5e540f2dab62d06
sarahnymann <saj004@edu.zealand.dk>
2026-03-16 11:42:33 +0100

    Add checkout page and navigation from cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,6 +2,7 @@
 
 import { useCart } from "@/context/CartContext";
 import CartItem from "@/components/CartItem";
+import Link from "next/link";
 
 export default function CartPage() {
   const { cart } = useCart();
@@ -13,11 +14,22 @@ export default function CartPage() {
       {cart.length === 0 ? (
         <p>Din kurv er tom.</p>
       ) : (
+        <> {/* Bruger et Fragment (<> </>) til at samle flere elementer uden at tilføje en ekstra <div> i HTML. */}
         <div className="space-y-4">
           {cart.map((item) => (
             <CartItem key={item.idDrink} item={item} />
             ))}
         </div>
+
+        <div className="mt-6 flex justify-end">
+          <Link
+            href="/checkout"
+            className="inline-block rounded-xl bg-pink-950 px-5 py-3 text-yellow-100 hover:opacity-90">
+              Gå til checkout
+            </Link>
+        </div>
+        </>
+        
       )}
     </section>
   );
---
+++ app/checkout/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import { useCart } from "@/context/CartContext";
+
+export default function CheckoutPage() {
+    const { cart } = useCart();
+
+    if (cart.length === 0) {
+        return <p>Din kurv er tom.</p>;
+        }
+
+    return (
+        <section className="max-w-2xl mx-auto p-6">
+            <h1 className="text-3xl font-bold mb-6">Checkout</h1>
+
+            <div className="divide-y">
+            {cart.map((item) => (
+              <article
+                key={item.idDrink}
+                className="flex items-center justify-between p-4"
+              >
+                <img
+                  src={item.strDrinkThumb}
+                  alt={item.strDrink}
+                  className="h-24 w-24 rounded-lg object-cover"
+                />
+
+                <div>
+                  <h2 className="text-lg font-semibold">{item.strDrink}</h2>
+                  <p className="text-sm text-gray-600">Antal: {item.quantity}</p>
+                </div>
+              </article>
+            ))}
+          </div>
+
+          <div className="mt-8 flex justify-end gap-3">
+            <Link
+              href="/cart"
+              className="rounded-full border-2 border-pink-950 px-4 py-2 text-pink-950 transition hover:bg-yellow-100"
+            >
+              Tilbage til kurv
+            </Link>
+
+            <button className="rounded-full bg-pink-950 text-yellow-100 px-4 py-2 hover:opacity-90">
+            Bestil drinks
+          </button>
+          </div>
+        </section>
+    )
+}
